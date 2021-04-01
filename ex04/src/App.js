32d9f779dcf981518b66b8b35a218e513f5665cd
berinko <berin@live.com>
2021-04-01 09:59:28 +0200

    zadatak cetvrti

--- ex04/src/App.js
+++ ex04/src/App.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+   
+  setLocalStorage = () => {
+    localStorage.setItem("Arena", "Selection Month");
+  }
+  
   getLocalStorage = () => {
     let myLocalStorageData = localStorage.getItem("Arena");
   }
+  
   render() {
     return(
       <div className="App">
-        <button onClick={this.getLocalStorage}>Button1</button>
+        <button onClick={this.setLocalStorage}>Button1</button>
+        <button onClick={this.getLocalStorage}>Button2</button>
       </div>
     )
   }
 }
+
+export default App;
